3e52a66369c9d90a71ee57b8479f228078a8f658
alexmjd <majda_a@etna-alternance.net>
2022-08-16 10:30:26 -0400

    [Feature] Insert some data to prepare future tests.

--- database/db.sql
+++ database/db.sql
@@ -4,4 +4,10 @@ CREATE TABLE IF NOT EXISTS product (
 	id int not null primary key auto_increment,
 	name varchar(50) not null,
 	price decimal(5,2) not null
-)
+);
+
+INSERT INTO product VALUES (1, "Table", 20.29);
+INSERT INTO product VALUES (2, "Chaise", 11.1);
+INSERT INTO product VALUES (3, "Lit", 499.49);
+INSERT INTO product VALUES (4, "Bureau", 300);
+INSERT INTO product VALUES (5, "Coffre", 999.99);
