04c8125c0fd2827e5a15db5fb937f7eb295e8cf2
alexmjd <majda_a@etna-alternance.net>
2022-08-21 10:58:57 -0400

    [Fix] Fixing insert statements.

--- database/db.sql
+++ database/db.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS products (
 	price decimal(5,2) not null
 );
 
-INSERT INTO product VALUES (1, "Table", 20.29);
-INSERT INTO product VALUES (2, "Chaise", 11.1);
-INSERT INTO product VALUES (3, "Lit", 499.49);
-INSERT INTO product VALUES (4, "Bureau", 300);
-INSERT INTO product VALUES (5, "Coffre", 999.99);
+INSERT INTO products VALUES (1, "Table", 20.29);
+INSERT INTO products VALUES (2, "Chaise", 11.1);
+INSERT INTO products VALUES (3, "Lit", 499.49);
+INSERT INTO products VALUES (4, "Bureau", 300);
+INSERT INTO products VALUES (5, "Coffre", 999.99);
