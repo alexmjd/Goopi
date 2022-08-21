247618e0d9e889c41aead0188e89258bf031a334
alexmjd <majda_a@etna-alternance.net>
2022-08-21 08:01:11 -0400

    [Feature] Moving all db connection code to the app.go file, splitting the app code.

--- main.go
+++ main.go
@@ -2,50 +2,19 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
 	"os"
-	"time"
-
-	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
 func main() {
 
-	// Use of mysql.NewConfig() to take all default parameters such as mysql_native_password
-	config := mysql.NewConfig()
+	a := App{}
+	a.Init(
+		os.Getenv("DBUSER"),
+		os.Getenv("DBPASS"),
+		os.Getenv("DBNAME"),
+	)
 
-	config.User = os.Getenv("DBUSER")
-	config.Passwd = os.Getenv("DBPASS")
-	config.Net = "tcp"
-	config.Addr = "godb:3306"
-	config.DBName = "godb"
-
-	// Loop the DB ping until it works.
-	loop := true
-	for loop {
-
-		db, err := sql.Open("mysql", config.FormatDSN())
-
-		if err != nil {
-			log.Fatal("Error while connecting to DB")
-		}
-
-		log.Print("SQL Connection openned...")
-
-		pingErr := db.Ping()
-		if pingErr != nil {
-			log.Println("Ping Error: ", pingErr)
-		}
-
-		if err == nil && pingErr == nil {
-			loop = false
-		}
-
-		time.Sleep(time.Second * 3)
-	}
-
-	fmt.Println("Hello db")
+	a.Run(":9999")
 }
