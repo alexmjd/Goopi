e2d263def32cbc44126bfd651f28e58a4edd8c18
alexmjd <majda_a@etna-alternance.net>
2022-08-28 05:22:34 -0400

    [Feature] Implementing CreateProduct. Add some more CRUD Functions.

--- src/models/product.go
+++ src/models/product.go
@@ -12,9 +12,27 @@ type Product struct {
 }
 
 func (p *Product) GetProduct(db *sql.DB) error {
-	return db.QueryRow("SELECT name, price FROM products WHERE id=?", p.Id).Scan(&p.Name, &p.Price)
+	return db.QueryRow("SELECT name, price FROM products WHERE id=?;", p.Id).Scan(&p.Name, &p.Price)
+}
+
+func (p *Product) GetAllProduct(db *sql.DB) error {
+	return db.QueryRow("SELECT * FROM products;").Scan(&p)
 }
 
 func (p *Product) CreateProduct(db *sql.DB) error {
-	return errors.New("Not Implemented")
+	err := db.QueryRow("INSERT INTO products(name, price) VALUES($1, $2) RETURNING ID", p.Name, p.Price).Scan(&p.Id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Product) UpdateProduct(db *sql.DB) error {
+	return errors.New("Not Implemented yet.")
+}
+
+func (p *Product) DeleteProduct(db *sql.DB) error {
+	return errors.New("Not Implemented yet.")
 }
